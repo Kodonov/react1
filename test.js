deea355f29958b20e7410b689fb66976438ab492
Kodonov <93920158+Kodonov@users.noreply.github.com>
2022-09-13 18:18:36 +0300

    front

---
+++ test.js
@@ -0,0 +1,13 @@
+function Test () {
+    const test = 'test React'
+
+    return (
+        <div>
+            <h1>GOOD BOYS</h1>
+            <p>{test}</p>
+        </div>
+    )
+}
+
+export default Test;
+
